b80993ca26178c62850d5c576146f4f0e0c16cb2
tamasvegh-nov11 <ttamas.vegh@gmail.com>
2026-09-19 06:24:23 +0200

    Create layout.js

---
+++ app/how-it-works/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "How It Works",
+  description:
+    "See how Feed Me Budapest matches Budapest landmarks, food preferences, walking distance and live opening hours to a short list of restaurant recommendations.",
+  alternates: {
+    canonical: "/how-it-works",
+  },
+};
+
+export default function HowItWorksLayout({ children }) {
+  return children;
+}
